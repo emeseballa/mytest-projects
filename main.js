bab1616d534c0fcc029c8a3751e55e1080da0d36
emeseballa <58734409+emeseballa@users.noreply.github.com>
2020-05-21 17:37:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+let name = 'Mesi';
+console.log(name);
+
+
+let firstName = 'Emese'; 
+let lastName = 'Balla';
+
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = 'Mesi';
+let age = 30;
+let isApproved = true;
+let selectedColor = null;
+
+
+let name = 'Mesi';
+let age = 30;
+
+
+let person = {
+    name: 'Mesi',
+    age: 30
+}
+
+console.log(person);
+
+
+function greet(name) {
+    console.log('Szia ' + name);
+}
+
+greet('Mesi');
